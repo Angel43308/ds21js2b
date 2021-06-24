125079cdf74525a24ca0358ce12e513444ac50d5
Angel43308 <79113623+Angel43308@users.noreply.github.com>
2021-06-24 01:01:41 -0500

    Delete 4searching directory

--- 4searching/search.js
+++
@@ -1,9 +0,0 @@
- let d = ["Angel","Armando","Rojas","Herrera"];
-let k = "Armando";
-let i = 0;
-  for (i = 0; i<=d.length; i++){
-  if (k==d[i])
-  console.log(k+" en Posicion: "+i);
-    
-    else if ((i==d.length)&&(d[i]!=k))
-  console.log("-1");}
